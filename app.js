d438d49e595cd306396aeeb5bcbcb3d4c35b22b7
Aland Mariwan <dev@aland-mariwan.de>
2023-02-18 17:16:00 +0100

    auto commit

--- app.js
+++ app.js
@@ -84,7 +84,6 @@ list.addEventListener("drop", (e) => {
 
 const itemMarkup = (file, url, x, y) =>
 {
-    console.log(url)
   const item = document.createElement("div");
   const id = Math.random().toString(36).substr(2, 9);
 
